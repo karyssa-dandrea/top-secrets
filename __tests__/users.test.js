20fb9ef446c8363f18d76dae64a6cd02f04e77f1
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-29 14:33:50 -0700

    created user model and fixed errors for create test to pass

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -20,14 +20,13 @@ describe('top-secrets routes', () => {
   });
 
   it('creates a new user', async () => {
-    const res = await request(app).post('api/v1/users').send(mockUser);
-    const { firstName, lastName, email, password } = mockUser;
+    const res = await request(app).post('/api/v1/users').send(mockUser);
+    const { firstName, lastName, email } = mockUser;
     expect(res.body).toEqual({
       id: expect.any(String),
       firstName,
       lastName,
       email,
-      password,
     });
   });
 });
