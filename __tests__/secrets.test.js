ba1db826f5994f428a941c684ef2d1f20b898c3c
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-29 17:03:41 -0700

    created creat secret test

---
+++ __tests__/secrets.test.js
@@ -0,0 +1,28 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('top-secrets routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('creates a secret', async () => {
+    const expected = {
+      title: 'Nori and Tokio',
+      description: 'the cutest pups',
+    };
+    const res = await request(app).post('/api/v1/secrets').send(expected);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...expected,
+      createdAt: expect.any(String),
+    });
+  });
+});
