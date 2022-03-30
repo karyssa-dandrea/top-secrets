078b0122646e86d5a3eb38865fc5c35307d3fb19
karyssa-dandrea <karyssadandrea@gmail.com>
2022-03-30 15:18:07 -0700

    wrote get all secrets test

--- __tests__/secrets.test.js
+++ __tests__/secrets.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Secret = require('../lib/models/Secret');
 
 describe('top-secrets routes', () => {
   beforeEach(() => {
@@ -24,5 +25,12 @@ describe('top-secrets routes', () => {
       ...expected,
       createdAt: expect.any(String),
     });
+  });
+
+  it('gets list of secerets', async () => {
+    const expected = await Secret.findAll();
+    const res = await request(app).get('/api/v1/secrets');
+
+    expect(res.body).toEqual(expected);
   });
 });
